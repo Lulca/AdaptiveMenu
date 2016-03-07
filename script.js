8de28dd42bc54bc2c0db75f5748d9072f8800fd1
Lulca <Anatolkruv@gmail.com>
2016-03-07 20:44:37 +0200

    Own tabs menu

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-$(function(){
-	$('.tab-content>div:not(":first-of-type")').hide();
-	$('<div class="line"></div>').appendTo('.tab-menu li');
-	$('.tab-menu li:first-of-type').find(':first').width('100%');
+$(function () {
+	var tabMenu = $('.tab-menu li');
+	var tabContent = $('.tab-content>div');
 
-	$('.tab-menu li').each(function(i){
-		$(this).attr('data-tab', 'tab'+i);
-	});
+	tabContent.not(':first-of-type').hide();
 
-	$('.tab-content>div').each(function(i){
-		$(this).attr('data-tab', 'tab'+i);
-	});
-
-	$('.tab-menu li').on('click', function(){
-		var dataTab = $(this).data('tab');
-		var getWrapper = $(this).closest('.tab-wrapper');
-		var line = $(this).find('.line');
-		getWrapper.find('.tab-menu li').removeClass('active');
+	tabMenu.on('click', function(){
+		$(this).siblings().removeClass('active');
 		$(this).addClass('active');
 
-		getWrapper.find('.line').width(0);
-		line.animate({'width':'100%'}, 200);
-		getWrapper.find('.tab-content>div').hide();
-		getWrapper.find('.tab-content>div[data-tab='+dataTab+']').show();
-	});
+		var wrapper = $(this).closest('.tab-wrapper');
+
+		var wrapTabContentDiv = wrapper.find('div').not(':first');
+		var wrapTabContent = wrapper.find('.tab-content');
+
+		wrapTabContentDiv.hide();
+
+		var findAttr =  $(this).attr('data-tab');
+		wrapTabContent.find('[data-tab="'+findAttr+'"]').show();
+	
+	}); 
 });
