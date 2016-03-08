2c27293e8db25b01aaa825a97f9bbe5bd5d1785e
Lulca <Anatolkruv@gmail.com>
2016-03-08 18:12:46 +0200

    add new folder whith img

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-$(function () {
-	var width = $('#slider-container').width();
-	$('.slides>li').width(width);
-	$('.slides').width(width*$('.slides>li').length);
-	$('.slides').css('left', -width);
-	$('.slides>li:last-child').prependTo('.slides');
+$(function() {
+	var slides = $('.slides li');
+	var slideWidth = slides.width(); 
 
-	function nextSlide() {
-		$('.slides').animate({
-			'margin-left':-width
-		}, 500, function() {
-			$('.slides>li:first-child').appendTo('.slides');
-			$('.slides').css('margin-left', 0);
-		});
-	}
-
-	function prevSlide() {
-		$('.slides').animate({
-			'margin-left':width
-		}, 500, function() {
-			$('.slides>li:last-child').prependTo('.slides');
-			$('.slides').css('margin-left', 0);
-		});
-	}
-
-
-	$('.next').click(nextSlide);
-	$('.prev').click(prevSlide);
+	// console.log(slides);
+	// console.log(slideWidth);
+	// $('#slider-wrap').width(slideWidth*slides.length);
 });
