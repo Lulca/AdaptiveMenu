30af0aff4c668d534241cc920674155d7e9f1db7
Lulca <Anatolkruv@gmail.com>
2016-03-08 21:48:21 +0200

    Try create adaptive img and float it

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(function() {
 	var slides = $('.slides li');
 	var slideWidth = slides.width(); 
 
-	// console.log(slides);
-	// console.log(slideWidth);
-	// $('#slider-wrap').width(slideWidth*slides.length);
+	// $('.slides').width(slideWidth*slides.length);
+	console.log(slideWidth*slides.length);
 });
